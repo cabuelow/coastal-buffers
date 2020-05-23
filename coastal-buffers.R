5d6d80b4dd1a4dbf7e34f076549b553c8605ef0c
cabuelow <56636458+cabuelow@users.noreply.github.com>
2020-05-23 11:56:37 +1000

    Update coastal-buffers.R

--- coastal-buffers.R
+++ coastal-buffers.R
@@ -7,7 +7,7 @@ library(tidyverse)
 # data 
 catch <- st_read('Catchments/QLD_Catchments_trans.gpkg')
 crop <- st_bbox(c(xmin = 141, xmax = 144, ymax = -10.5, ymin = -13), crs = st_crs(4326))
-fnqld <- st_crop(catch, crop) %>% st_transform(crs = 4283)
+fnqld <- st_crop(catch, crop) %>% st_transform(crs = 4326)
 
 # coastal buffers
 qld.buff <- st_difference(st_union(st_buffer(qld, dist = 0.1)))
